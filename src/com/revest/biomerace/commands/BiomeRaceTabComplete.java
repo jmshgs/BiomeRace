01d9b341a8d169e08aa6daf28dae73fae5b95b3b
JackTGG <zackgozelski@icloud.com>
2021-09-08 21:03:31 -0700

    Update BiomeRaceTabComplete.java

--- src/com/revest/biomerace/commands/BiomeRaceTabComplete.java
+++ src/com/revest/biomerace/commands/BiomeRaceTabComplete.java
@@ -11,14 +11,14 @@ public class BiomeRaceTabComplete implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String string, String[] strings) {
         if (cmd.getName() == "updatedelay") {
-            if (strings.length == 1) {
-                ArrayList<String> values = new ArrayList<String>();
+            if (strings.length > 0) {
+                ArrayList<String> values = new ArrayList<>();
 
 
                 values.add("ab");
                 values.add("rc");
 
-                Collections.sort(values);
+                //Collections.sort(values);
 
                 return values;
             }
