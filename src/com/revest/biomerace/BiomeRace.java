9d9ea453f705e3fb8f1dc7c5a904b6af928347cf
JackTGG <zackgozelski@icloud.com>
2021-09-08 21:06:02 -0700

    add Tab Completer to Command

--- src/com/revest/biomerace/BiomeRace.java
+++ src/com/revest/biomerace/BiomeRace.java
@@ -1,5 +1,6 @@
 package com.revest.biomerace;
 
+import com.revest.biomerace.commands.BiomeRaceTabComplete;
 import com.revest.biomerace.events.BiomeRaceEvents;
 import com.revest.biomerace.commands.BiomeRaceCommands;
 import org.bukkit.ChatColor;
@@ -13,6 +14,7 @@ public class BiomeRace extends JavaPlugin {
     @Override
     public void onEnable() {
         BiomeRaceCommands commands = new BiomeRaceCommands(this);
+        BiomeRaceTabComplete tabComplete = new BiomeRaceTabComplete(this);
         getServer().getPluginManager().registerEvents(new BiomeRaceEvents(), this);
         getCommand("biome").setExecutor(commands);
         getCommand("pplonline").setExecutor(commands);
@@ -20,6 +22,7 @@ public class BiomeRace extends JavaPlugin {
         getCommand("stoprace").setExecutor(commands);
         getCommand("racestatus").setExecutor(commands);
         getCommand("updatedelay").setExecutor(commands);
+        getCommand("updatedelay").setTabCompleter(tabComplete);
         getServer().getConsoleSender().sendMessage("§b[BiomeRace]: Plugin is enabled!");
         //this.saveDefaultConfig();
 
