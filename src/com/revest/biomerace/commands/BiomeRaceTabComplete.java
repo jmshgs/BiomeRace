1ee424f4d628bbf01667e5493c59f7daf92e8e62
JackTGG <zackgozelski@icloud.com>
2021-09-08 19:58:16 -0700

    adding to the commands tab.

--- src/com/revest/biomerace/commands/BiomeRaceTabComplete.java
+++ src/com/revest/biomerace/commands/BiomeRaceTabComplete.java
@@ -4,31 +4,33 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BiomeRaceTabComplete implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command cmd, String string, String[] strings) {
-        if (cmd.getName() == "actionbarupdatedelay") {
+        if (cmd.getName() == "updatedelay") {
             if (strings.length == 1) {
                 ArrayList<String> values = new ArrayList<String>();
 
-                if (strings[0].equals("")) {
 
-                }
+                values.add("ab");
+                values.add("rc");
+
+                Collections.sort(values);
+
+                return values;
             }
+
         }
-
-
-
-
-
-
-
-
-
         return null;
     }
 }
+
+
+
+
+
+
+
