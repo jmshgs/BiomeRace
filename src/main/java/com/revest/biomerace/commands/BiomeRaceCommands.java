e3af3aa03b44ca71847fd278a46e97c13e8954e1
dapl3b <jameshug678@gmail.com>
2021-10-02 07:11:51 -0700

    added maven in order to use papermc api

--- src/main/java/com/revest/biomerace/commands/BiomeRaceCommands.java
+++ src/main/java/com/revest/biomerace/commands/BiomeRaceCommands.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
-
+import io.papermc.paper.world.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
