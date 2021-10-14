b8020e5bb2aceef06d905db905d595a01a9551f4
revestyt <jameshug678@gmail.com>
2021-10-13 21:09:06 -0700

    started added compass funcionality

--- src/main/java/com/revest/biomerace/events/BiomeRaceEvents.java
+++ src/main/java/com/revest/biomerace/events/BiomeRaceEvents.java
@@ -1,11 +1,16 @@
 package com.revest.biomerace.events;
 
 import com.revest.biomerace.BiomeRace;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
+import static org.bukkit.Bukkit.getServer;
 
 
 public class BiomeRaceEvents implements Listener {
@@ -16,8 +21,23 @@ public class BiomeRaceEvents implements Listener {
 
     @EventHandler
     public static void onPlayerJoin(PlayerJoinEvent event) {
+        Player closeplayer = null;
+        boolean whilebool = true;
         Player player = event.getPlayer();
         player.sendMessage("§bWelcome! :D");
-    }
+        if (!player.getInventory().containsAtLeast(new ItemStack(Material.COMPASS), 1)) {
+            player.getInventory().addItem(new ItemStack(Material.COMPASS, 1));
+        }
+        while (whilebool = true) {
+            for (Player currentplayer : getServer().getOnlinePlayers()) {
+                if (currentplayer == player) {
+                    Location location = currentplayer.getLocation();
+                    Location location2 = player.getLocation();
+                    double distance = location.distance(location2);
 
+                }
+
+            }
+        }
+    }
 }
